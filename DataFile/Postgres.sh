e45b65d5f3ab7e5a60c6065fb4e83cdb8714d55a
Shossi <yossizxc@gmail.com>
2022-03-21 19:01:27 +0200

    First Commit - user Datafiles and Dockerfile added

---
+++ DataFile/Postgres.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# This script will run on vm creation and create a postgresql env using a dockerfile.
+sudo apt update -y
+sudo apt install -y docker.io
+sudo systemctl enable docker
+sudo docker run -d --name measurements --restart always -p 5432:5432 -e 'POSTGRES_PASSWORD=p@ssw0rd42' postgres
