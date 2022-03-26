261158a0d348777c3dbf702b0a048c48e0d7f566
Shossi <yossizxc@gmail.com>
2022-03-26 21:18:11 +0300

    minor changes - storage account wasnt in the root tf file

--- DataFile/WebApp.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo apt update
-sudo apt install docker.io -y1
-sudo systemctl enable docker
-sudo docker run -d --restart always -p 8080:8080 joeyhd/weight:1.1
--- vm_db_lb/providers.tf
+++ vm_db_lb/providers.tf
@@ -5,14 +5,7 @@ terraform {
       version = "=2.97.0"
     }
   }
-  backend "azurerm" {
-    resource_group_name  = "tfstate"
-    storage_account_name = "tfstatejoey"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-  }
 }
-
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
