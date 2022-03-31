14e7ccaf61284c16e2b0bdd88ca8b0fcab74b2a0
Shossi <yossizxc@gmail.com>
2022-03-31 20:28:48 +0300

    change docker file image to node-14-alpine to save space

--- DataFile/Postgres.sh
+++ DataFile/Postgres.sh
@@ -4,4 +4,4 @@ sudo apt update -y
 sudo apt install -y docker.io
 sudo systemctl enable docker
 sudo docker run -d --name measurements --restart always -p 5432:5432 -e 'POSTGRES_PASSWORD=p@ssw0rd42' postgres
-# << Change the password << This is an example passowrd
+# ^^ Change the password ^^ This is an example! ^^
